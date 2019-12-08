74712b2a1485a19706b7285677c954afa487ecae
Raymond Hill <infamous.rj@gmail.com>
2019-12-08 14:27:05 -0500

    Bloop.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use math::round;
 
 fn main() -> std::io::Result<()> {
     // read this ol dingus input file
@@ -9,30 +8,52 @@ fn main() -> std::io::Result<()> {
     // use the BufReader because it's buff as heck
     let buf_reader = BufReader::new(file);
 
-    let mut sum = 0.0;
+    let mut _output = vec![];
+    let mut arr = vec![];
 
     // loop this jank
     for line in buf_reader.lines() {
         let l = line.unwrap();
-        let x = l.parse::<f64>().unwrap();
-        let y = round::floor(x/3.0, 0)-2.0;
 
-        // let's make this real gross
-        let mut done = false;
-        let mut z = y;
-        while !done {
-            if z <= 5.0 {
-                done = true;
+        // split this dang string by comma
+        let split = l.split(",");
+        // shove the whole thing into a vector
+        for s in split {
+            // numbers... who needs em?
+            let num = s.parse::<i64>().unwrap();
+            arr.push(num);
+        }
+    }
+    // now I have vector to play with, yay!
+    _output = chug_numbers(arr);
+    println!("output: {:?}", _output);
+    Ok(())
+}
+
+fn chug_numbers(numbers: Vec<i64>) -> Vec<i64> {
+    println!("Initial vector: {:?}", numbers);
+    let mut arr = numbers.to_vec();
+    for n in 0..arr.len() {
+        if n%4==0 {
+            let op = numbers[n];
+            if op == 99 {
+                return arr;
             } else {
-                z = round::floor(z/3.0, 0)-2.0;
-                sum += z;
-                // println!("{}", z);
+                // this is gross and I don't care
+                let x_index = numbers[n+1] as usize;
+                let x = arr[x_index];
+                let y_index = numbers[n+2] as usize;
+                let y = arr[y_index];
+                let storage = numbers[n+3] as usize;
+
+                if op==1 {
+                    arr[storage] = x+y;
+                }
+                if op==2 {
+                    arr[storage] = x*y;
+                }
             }
         }
-
-        // println!("round({}/3)-2={}", x, y);
-        sum += y;
     }
-    println!("sum: {}", sum);
-    Ok(())
+    return arr;
 }
