f741ef91112d95b98a7fe83f770f39ed1152c488
Raymond Hill <infamous.rj@gmail.com>
2019-12-08 11:16:58 -0500

    Finished day1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use math::round;
 
-fn main() {
-    let stdout = stdout();
-    let out = b"SO MUCH FLIPPIN RUST!";
-    let width = 21;
+fn main() -> std::io::Result<()> {
+    // read this ol dingus input file
+    let file = File::open("input.txt")?;
+    // use the BufReader because it's buff as heck
+    let buf_reader = BufReader::new(file);
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    let mut sum = 0.0;
+
+    // loop this jank
+    for line in buf_reader.lines() {
+        let l = line.unwrap();
+        let x = l.parse::<f64>().unwrap();
+        let y = round::floor(x/3.0, 0)-2.0;
+
+        // println!("round({}/3)-2={}", x, y);
+        sum += y;
+    }
+    println!("sum: {}", sum);
+    Ok(())
 }
